919d75fff331ceb26a48cc5846a58d0d0b8410b3
javigalvan07 <javiergalvanromero@gmail.com>
2024-11-11 12:19:12 +0000

    Correccción del código Fibonacci

--- src/utils/fibojavier.js
+++ src/utils/fibojavier.js
@@ -2,9 +2,7 @@ export function fibonacci(num) {
     const fib = [0, 1];
 
     for (let i = 2; i <= num; i++) {
-        //fib[i] = fib[i - 1] + fib[i - 2];
-        fib[i] = fib[i - 1] + fib[i - 4];
-        fib[i] = fib[i - 1] + fib[i - r];
+        fib[i] = fib[i - 1] + fib[i - 2];
     }
 
     return fib[num];
