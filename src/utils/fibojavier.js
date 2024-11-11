0201a9e98046af2b2c6bc74ef4097dca2c52acb8
javigalvan07 <javiergalvanromero@gmail.com>
2024-11-11 12:17:41 +0000

    Botón hecho

--- src/utils/fibojavier.js
+++ src/utils/fibojavier.js
@@ -2,7 +2,9 @@ export function fibonacci(num) {
     const fib = [0, 1];
 
     for (let i = 2; i <= num; i++) {
-        fib[i] = fib[i - 1] + fib[i - 2];
+        //fib[i] = fib[i - 1] + fib[i - 2];
+        fib[i] = fib[i - 1] + fib[i - 4];
+        fib[i] = fib[i - 1] + fib[i - r];
     }
 
     return fib[num];
